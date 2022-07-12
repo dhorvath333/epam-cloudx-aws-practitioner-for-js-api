abbd17586d1941bfe54344415e84ac1b0969fed7
David Horvath <dhorvath333@gmail.com>
2022-07-12 19:54:24 +0200

    feat: configure serverless

---
+++ product-service/handler.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports.hello = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      productName: 'Book',
+      price: 12.99
+    })
+  };
+};
